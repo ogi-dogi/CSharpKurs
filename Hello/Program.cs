f2d2aeb49a79bc798f7d85969ede6035de312093
ogidogi <krzychu33b@gmail.com>
2019-06-02 18:15:46 +0200

    Pierwszy program po raz trzeci. Tablica args

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -7,11 +7,63 @@ namespace Hello
         static void Main(string[] args)
         {
             //string name = args[0];
-            //Console.WriteLine("Hello " + name);
-            foreach (var item in args)
-            {
-                Console.WriteLine("hello " + item);
-            }
+            //Console.WriteLine("Hello World! " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("witaj " + item);
+            //}
+            
         }
     }
 }
+
+/*using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+
+
+
+                Console.Write("Wpisz swoje imię ");
+                string name = Console.ReadLine();
+                //string name = args[0];
+                Console.WriteLine("Hello " + name);
+
+                Console.Write("Wpisz ile masz lat ");
+
+                int age;
+                bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if (age > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Brawo! JEsteś pełnoletni, możesz wypić browara");
+                }
+                else if (result == false)
+                {
+                    Console.WriteLine("Wprowadziłeś niepoprawny wiek, popraw to!");
+                }
+                else
+                {
+                    Console.WriteLine("Możemy Ci zaoferować mleko");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+            }
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("hello " + item);
+            //}
+        }
+    }
+} */
